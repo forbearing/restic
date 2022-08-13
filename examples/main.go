f49b3c11c6bd5d00bd533aaf11047a93b4fbc8ba
hybfkuf <fredjoseph863@gmail.com>
2022-08-13 15:26:22 +0800

    Add `restic snapshot` example

--- examples/main.go
+++ examples/main.go
@@ -18,6 +18,7 @@ var (
 func main() {
 	defer cancel()
 
+	// new a restic instance.
 	r, err := restic.New(ctx, &restic.GlobalFlags{
 		NoCache:       true,
 		LimitDownload: 20480,
@@ -27,26 +28,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// restic backup
 	r.Command(restic.Backup{
 		Host: "myhost",
 		Tag:  []string{"mytag", "test"},
 	}.SetArgs(backupSource1, backupSource2))
 	fmt.Println(r)
+	//r.Run() // execute `restic backup command`
 
-	r.Command(&restic.Backup{
-		Host: "myhost",
+	// restic backup
+	r.Command(&restic.Snapshots{
+		Host: []string{"myhost"},
 		Tag:  []string{"mytag"},
 	})
 	fmt.Println(r)
+	//r.Run() // execute `restic snapshots command`
 
+	// restic snapshots
 	r.Command(&restic.Forget{
 		KeepLast: 1,
 	})
 	fmt.Println(r)
+	//r.Run() // execute `restic forget command`
 
 	// Output:
 
 	//restic --limit-download=20480 --limit-upload=1024 --no-cache backup --host=myhost --tag=mytag,test ./testdata/backup_source_1 ./testdata/backup_source_2
-	//restic --limit-download=20480 --limit-upload=1024 --no-cache backup --host=myhost --tag=mytag
+	//restic --limit-download=20480 --limit-upload=1024 --no-cache snapshots --host=myhost --tag=mytag
 	//restic --limit-download=20480 --limit-upload=1024 --no-cache forget --keep-last=1
 }
