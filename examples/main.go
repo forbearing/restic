e4cbeb39121c39f9f042143a51ee106fe88e5a1f
hybfkuf <fredjoseph863@gmail.com>
2022-08-14 19:25:13 +0800

    Update examples

--- examples/main.go
+++ examples/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"github.com/forbearing/restic"
+	"github.com/sirupsen/logrus"
 )
 
 var (
 	ctx, cancel   = context.WithTimeout(context.Background(), time.Minute*10)
-	backupSource1 = "./testdata/backup_source_1"
-	backupSource2 = "./testdata/backup_source_2"
+	backupSource1 = "../testdata/backup_source_1"
+	backupSource2 = "../testdata/backup_source_2"
+	repo          = "/tmp/restic_repo"
 )
 
 func main() {
@@ -23,37 +27,39 @@ func main() {
 		NoCache:       true,
 		LimitDownload: 20480,
 		LimitUpload:   1024,
+		Repo:          repo,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+	writer := logrus.New().WriterLevel(logrus.ErrorLevel)
+	// discard all standard output and output error message to logrus's writer.
+	r.SetOutput(io.Discard, writer)
 
-	// restic backup
+	// execute `restic init` command
+	os.RemoveAll(repo)
+	r.Command(restic.Init{}).Run()
+	fmt.Println(r)
+
+	//execute `restic backup` command
 	r.Command(restic.Backup{
 		Host: "myhost",
 		Tag:  []string{"mytag", "test"},
-	}.SetArgs(backupSource1, backupSource2))
+	}.SetArgs(backupSource1, backupSource2)).Run()
 	fmt.Println(r)
-	//r.Run() // execute `restic backup command`
+	r.Run()
+	r.Run()
 
-	// restic backup
+	// execute `restic snapshots` command
 	r.Command(&restic.Snapshots{
 		Host: []string{"myhost"},
 		Tag:  []string{"mytag"},
-	})
+	}).Run()
 	fmt.Println(r)
-	//r.Run() // execute `restic snapshots command`
-
-	// restic snapshots
-	r.Command(&restic.Forget{
-		KeepLast: 1,
-	})
-	fmt.Println(r)
-	//r.Run() // execute `restic forget command`
 
 	// Output:
 
-	//restic --limit-download=20480 --limit-upload=1024 --no-cache backup --host=myhost --tag=mytag,test ./testdata/backup_source_1 ./testdata/backup_source_2
-	//restic --limit-download=20480 --limit-upload=1024 --no-cache snapshots --host=myhost --tag=mytag
-	//restic --limit-download=20480 --limit-upload=1024 --no-cache forget --keep-last=1
+	//restic --limit-download=20480 --limit-upload=1024 --no-cache --repo=/tmp/restic_repo init
+	//restic --limit-download=20480 --limit-upload=1024 --no-cache --repo=/tmp/restic_repo backup --host=myhost --tag=mytag,test ../testdata/backup_source_1 ../testdata/backup_source_2
+	//restic --limit-download=20480 --limit-upload=1024 --no-cache --repo=/tmp/restic_repo snapshots --host=myhost --tag=mytag
 }
